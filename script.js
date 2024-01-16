8f1abb06f17a9401a4be6215d426d4e8ed6b19ac
Dane knight <knight1seven@gmail.com>
2024-01-15 20:57:51 -0500

    need to add innerHTMl to addEntry function

--- script.js
+++ script.js
@@ -24,11 +24,21 @@ function isInvalidInput(str) {
 function addEntry() {
     const targetInputContainer = document.querySelector(`{targetId} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type= "text"]').length;
-    constHTMLString = `
+    const HTMLString = `
     <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
     <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
-    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>`;
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
+    targerInputContainer += document.getElementById("targetInputContainer").innerHTML += HTMLString;
+
 }
+
+
 
 
 
